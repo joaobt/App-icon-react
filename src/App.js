cd0886f72dcf590289f80f03211ae86d647afe05
Joao Batista <joao4729@gmail.com>
2022-07-21 16:46:50 -0300

    a.2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React, { useState }from 'react';
-import Icon from './Icon';
+import React, { useState } from 'react';
+import Switch from './components/Switch';
+
+const initialState = {
+  fat: false,
+  bald: false,
+  poor: false
+}
 
 
 const App = () => {
 
-  const [isOn,setIsOn] = useState(true);
+  const [switches, setSwitches] = useState(initialState);
 
-  const toggle = () => {
-    setIsOn(!isOn);
-  }
+  const toggleSwitch = id => {
+    const currentState = switches[id];
+    if (currentState === undefined) return;
 
-  return (
-    <div className="container">
-      <h1> Gordo, careca, pobre</h1>
-      
-      <div onClick={() => {toggle()}} className={`item ${isOn ? 'on' : 'off'}`}>
-        <label>Gordo</label> <Icon id="switch" />
-      </div>
-    </div>
-  );
+    const activeSwitches = Object.values(switches).filter(value => value).length;
+
+    const newState = (activeSwitches === 2 ? initialState : switches);
+
+    setSwitches({ ...newState, [id]: !currentState });
+
+  };
+return (
+  <div className="container">
+    <h1> Gordo, careca, pobre</h1>
+    <Switch onClick={toggleSwitch} id='fat' active={switches['fat']} label="Gordo" />
+    <Switch onClick={toggleSwitch} id='bald' active={switches['bald']} label="careca" />
+    <Switch onClick={toggleSwitch} id='poor' active={switches['poor']} label="pobre" />
+  </div>
+)
 };
 export default App;
+
+
+
+
+/*
+if (activeSwitches === 2) {
+  setSwitches({...initialState, [id]: !currentState});
+
+ } else {
+ console.log('currentState', id, currentState);
+
+setSwitches({ ...switches, [id]: !currentState });
+
+*/
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
 
 
 
